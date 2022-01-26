3123f5da2717e4adc85f8132da2e66a2a87d1690
razvan.mogosanu <razvan.mogosanu1@gmail.com>
2022-01-26 13:26:06 +0200

    init commit backend

---
+++ sanatero-api/src/main/java/com/example/sanateroapi/SanateroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanateroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanateroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanateroApiApplication.class, args);
+    }
+
+}
