c6474c8c490854056f40164dcc78d1a349916bb1
razvan.mogosanu <razvan.mogosanu1@gmail.com>
2022-11-11 08:07:41 +0200

    Add error handling for null medicalRecord

--- sanatero-api/src/main/java/com/example/sanateroapi/services/MedicalService.java
+++ sanatero-api/src/main/java/com/example/sanateroapi/services/MedicalService.java
@@ -1,13 +1,17 @@
 package com.example.sanateroapi.services;
 
+import com.example.sanateroapi.controllers.UserController;
 import com.example.sanateroapi.models.MedicalRecord;
 import com.example.sanateroapi.repositories.MedicalAssistanceRepository;
 import com.example.sanateroapi.repositories.MedicalRecordRepository;
 import com.example.sanateroapi.repositories.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MedicalService {
+    private static final Logger logger = LogManager.getLogger(MedicalService.class);
     private final MedicalRecordRepository medicalRecordRepository;
     private final UserRepository userRepository;
     private final MedicalAssistanceRepository medicalAssistanceRepository;
@@ -19,10 +23,14 @@ public class MedicalService {
     }
 
     public void addMedicalRecord(MedicalRecord medicalRecord) {
-        medicalRecord.setCaregiver(userRepository.getByUsername(medicalRecord.getCaregiver().getUsername()).get());
-        medicalRecord.setPatient(userRepository.getByUsername(medicalRecord.getPatient().getUsername()).get());
-        medicalRecord.setMedicalAssistance(medicalAssistanceRepository.getByName(medicalRecord.getMedicalAssistance().getName()).get());
-        medicalRecordRepository.save(medicalRecord);
+        try {
+            medicalRecord.setCaregiver(userRepository.getByUsername(medicalRecord.getCaregiver().getUsername()).get());
+            medicalRecord.setPatient(userRepository.getByUsername(medicalRecord.getPatient().getUsername()).get());
+            medicalRecord.setMedicalAssistance(medicalAssistanceRepository.getByName(medicalRecord.getMedicalAssistance().getName()).get());
+            medicalRecordRepository.save(medicalRecord);
+        } catch (NullPointerException e) {
+            logger.error("medical record is null", e);
+        }
     }
 
 }
